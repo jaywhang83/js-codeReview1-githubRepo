885b8d14ae607c1a80564cb73c03d12ca0bf2089
Jay Whang <jaywhang83@gmail.com>
2016-03-18 11:41:42 -0700

    Makes each repository divs clickable

--- js/getRepo-interface.js
+++ js/getRepo-interface.js
@@ -4,12 +4,11 @@ exports.getRepo = function(userName) {
   var githubRepo;
   $.get('https://api.github.com/users/'+ userName + "/repos"+'?access_token=' + apiKey).then(function(response) {
     githubRepo = response;
-    $('#userInfo').html("<img src= '" + response[0].owner.avatar_url +"'>");
+    $('#userInfo').html("<div class ='picture col-md-4'>" + "<img src= '" + response[0].owner.avatar_url +"'>" + "</div>");
     response.forEach(function(repo) {
-      console.log(repo);
-        $('#repos').append("<div class='col-md-4 box'>" + "<h4>Name: " + repo.name + "</h4>" + "<br />"
+        $('#repos').append("<a href ='" + repo.html_url + "'>" + "<div class='col-md-4 box'>" + "<h4>Name: " + repo.name + "</h4>" + "<br />"
         + "<p>Created at: "+ moment(repo.created_at).format("MM/DD/YYYY") + "</p>" + "<br />"
-        + "<p>Description: " + (repo.description === "" ? "n/a" : repo.description) + "</p>"  + "<br />"+ "</div>");
+        + "<p>Description: " + (repo.description === "" ? "n/a" : repo.description) + "</p>"  + "<br />"+ "</div>" + "</a>");
     });
   }).fail(function(error) {
 
