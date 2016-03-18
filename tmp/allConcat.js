e4aa935f9e888533beb14ba72e6ec0ea962fa91f
Jay Whang <jaywhang83@gmail.com>
2016-03-18 15:06:18 -0700

    Add more styling

--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -17,23 +17,22 @@ exports.getRepo = function(userName) {
   $.get('https://api.github.com/users/'+ userName +'?access_token=' + apiKey)
   .then(function(response) {
     profile = response;
-    return $.get('https://api.github.com/users/'+ userName +"/repos" +'?access_token=' + apiKey)})
+    return $.get('https://api.github.com/users/'+ userName +'/repos?access_token=' +  apiKey)})
     .then(function(repository) {
-    $('#userInfo').html("<div class ='picture col-md-4'>" +
-                          "<img src= '" + profile.avatar_url +"'>"
-                          + "<p>" + profile.name + "</p>"
+      console.log(repository);
+    $('#userInfo').html("<div class ='picture col-md-3'>" +
+                          "<img src= '" + profile.avatar_url +"'></div>"
+                          + "<div class='info col-md-3'><div class='profile'><p>" + profile.name + "</p>"
                           + "<p>" + profile.email + "</p>"
                           + "<a href ='" + profile.followers_url + "'>" + "Follwers: " + profile.followers + "</a> "
                           + " <a href='" + profile.followers_url + "'>" + "Following: " + profile.following + "</a>"
-                        + "</div>");
+                          + "</div></div>");
     repository.forEach(function(repo) {
         $('#repos').append("<a href ='" + repo.html_url + "'>"
                               + "<div class='col-md-4 box'>"
                                 + "<h4>Name: " + repo.name + "</h4>"
-                                + "<br />"
-                                + "<p>Created at: "+ moment(repo.created_at).format("MM/DD/YYYY") + "</p>" + "<br />"
+                                + "<p>Created on: "+ moment(repo.created_at).format("MM/DD/YYYY") + "</p>"
                                 + "<p>Description: " + (repo.description === "" ? "n/a" : repo.description) + "</p>"
-                                + "<br />"
                               + "</div>"
                             + "</a>");
     });
