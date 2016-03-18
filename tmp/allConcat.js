5247015dbb5189255d52c88027259a13894cf03e
Jay Whang <jaywhang83@gmail.com>
2016-03-18 09:02:35 -0700

    Add ajx call

--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -16,6 +16,7 @@ var apiKey = require('./../.env').apiKey;
 exports.getRepo = function(userName) {
   $.get('https://api.github.com/users/' + userName + '?access_token=' + apiKey).then(function(response) {
     console.log(JSON.stringify(response));
+    $('#userInfo').html("<img src= '" + response.avatar_url +"'>" +"</a>" + <br>); 
   }).fail(function(error) {
     console.log(error.responseJSON.message);
   });
